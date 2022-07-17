ae048dd4ffa7ec22e49a95378bfe902f97d8278a
Adam Fuzum Tewelde <a.tewelde@kugu-home.com>
2022-07-17 17:15:28 +0200

    complete scafold

---
+++ src/common/header/Header.js
@@ -0,0 +1,10 @@
+
+import * as React from "react";
+
+const  Header = () => {
+  return (
+   <div></div>
+  );
+}
+
+export default  Header;
---
+++ src/screens/home/Home.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
