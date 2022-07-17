ad3616c6fc5f40f88b252abd00420dca125501b6
Adam Fuzum Tewelde <a.tewelde@kugu-home.com>
2022-07-17 20:07:13 +0200

    add header intial component

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,10 +1,43 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
 
-import * as React from "react";
+import "./Header.css";
+import logo from "../../assets/logo.svg";
 
-const  Header = () => {
+const Header = (props) => {
+  const modalOpenHandler = (event) => { console.log("not yet implemented")};
+  const logoutHandler = (event) => {console.log("not yet implemented")};
+
   return (
-   <div></div>
+    <header>
+      <nav className="app-header">
+        <img src={logo} className="app-logo" alt="Movies App Logo" />
+        <div className="login-button">
+          <Button
+            variant="contained"
+            color="default"
+            onClick={modalOpenHandler}
+          >
+            Login
+          </Button>
+        </div>
+        <div className="login-button">
+          <Button variant="contained" color="default" onClick={logoutHandler}>
+            Logout
+          </Button>
+        </div>
+        <div className="bookshow-button">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={modalOpenHandler}
+          >
+            Book Show
+          </Button>
+        </div>
+      </nav>
+    </header>
   );
-}
+};
 
-export default  Header;
+export default Header;
