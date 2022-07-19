8f32a71241a3cc291aede9bdbc02ff940db0fafe
Adam Fuzum Tewelde <a.tewelde@kugu-home.com>
2022-07-19 13:15:56 +0200

    install redux dependecy

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
 
+import Header from '../common/header/Header';
+
 const Controller = () => {
   const baseUrl = "/api/v1/";
 
   return (
     <Router>
       <div className="main-container">
+      <Header></Header>
         <Route
           exact
           path="/"
